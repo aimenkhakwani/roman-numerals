23b4a3f0824b062eefd92b77dc1ceaabe3d6334f
Aimen Khakwani and Ewa Manek <ewa@>
2016-08-18 09:31:23 -0700

    complete 4th spec to return roman numeral for 2 and 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,27 @@
 //<!-- Back End -->
- function numberConvert(steve) {
-   var fiveTens = ["1", "5", "10", "50", "100", "500", "1000"];
-   var romanSymbols=["I", "V", "X", "L", "C", "D", "M"];
-   if(steve === "0"){
-     return steve;
-   } else if (parseInt(steve) > 3999) {
-     return "Get down from your high horse lol";
-   } else if (fiveTens.includes(steve)){
-     for(var i = 0; i < fiveTens.length; i++){
-       if(fiveTens[i] === steve){
-         return romanSymbols[i];
-       }
-     }
-   }
- }
+function numberConvert(steve) {
+  var fiveTens = ["1", "5", "10", "50", "100", "500", "1000"];
+  var romanSymbols=["I", "V", "X", "L", "C", "D", "M"];
+  if(steve === "0"){
+    return steve;
+  } else if (parseInt(steve) > 3999) {
+    return "Get down from your high horse lol";
+  } else if (fiveTens.includes(steve)){
+      for(var i = 0; i < fiveTens.length; i++){
+        if(fiveTens[i] === steve){
+          return romanSymbols[i];
+        }
+      }
+    //more efficient way with indexOf (for future reference)
+    //return romanSymbols[fiveTens.indexOf(steve)];
+  } else if(parseInt(steve) < 4){
+    var romNum = "I";
+    for(var i = 2; i <= parseInt(steve); i++){
+      romNum += "I";
+    }
+    return romNum;
+  }
+}
 
 //<!-- Front End  -->
 $(document).ready(function(){
