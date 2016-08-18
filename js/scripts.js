183f22095d56312f0e46484642dd6131889ead84
Aimen Khakwani and Ewa Manek <ewa@>
2016-08-18 09:00:07 -0700

    complete first spec to return 0

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,16 @@
 //<!-- Back End -->
-
+ function numberConvert(steve) {
+   if(steve === "0"){
+     return steve;
+   }
+ }
 
 //<!-- Front End  -->
 $(document).ready(function(){
   $("form#inputForm").submit(function(event){
     event.preventDefault();
-
+    var userInput = $("input#input").val();
+    var result = numberConvert(userInput)
+    $("#result").text(result);
   });
 });
